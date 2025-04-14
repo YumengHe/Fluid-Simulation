a0cc8c149e3c279f9c959609c998a989d36f657e
Yumeng He <2684438857@qq.com>
2025-04-14 14:09:21 -0700

    add more parameters to grid.h

--- src/grid.h
+++ src/grid.h
@@ -1,11 +1,18 @@
 #pragma once
 #include <vector>
+#include <include/eigen-3.4.0/Eigen/Dense>
 
 class Fluid_Grid{
 public:
-    int grid_w; //width of grid (x direction)
-    int grid_h; // height of grid (y direction)
-    float cell_size;
+    // grid dimension
+    int grid_w; // number of cells in horizontal direction
+    int grid_h; // number of cells in vertical direction
+
+    // physics parameter
+    float dt; // time step
+    float diffusion; // coefficient for diffusion
+    float viscosity; // coefficient for viscosity
+    int num_iteration; // number of iteration for linear solver
 
     //stored at the boundary of the cell
     std::vector<std::vector<float>> velocity_x;// Velocity in the x direction
@@ -16,14 +23,23 @@ public:
     std::vector<std::vector<float>> density;
 
     //Constructor
-    Fluid_Grid(int width,int height,float cell_size);
+    Fluid_Grid(int width,int height) {
+        
+    };
 
     //Initialize
     void initializeGrid();
 
     //add core function
+    
     //Step 1: Advection（对密度和速度进行推进）
+    void advect();
     //Step 2: Apply Forces
+    void diffuse();
     //Step 3: Projection（解 Poisson 方程）  
+    void project();
+    // apply boundary conditions (e.g. walls)
+    void setBoundary();
 
+    void simulation();
 };
